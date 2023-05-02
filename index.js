52176f149f950dac5053b4c173f259fe79dc28ba
tamzid620 <tamzid620@gmail.com>
2023-05-03 02:37:30 +0600

    chef find data added

--- index.js
+++ index.js
@@ -1,3 +1,28 @@
+// const express= require('express');
+// const cors = require('cors');
+
+// const app = express();
+// app.use(cors());
+
+// const chefs = require("./data.json");
+
+// app.get('/', (req , res) => {
+//     res.json({ message: "hello Server"});
+// });
+// app.get('/chefs/:id', (req, res) => {
+//     const id = req.params.id;
+//     console.log(id);
+//     const selectedChefs = chefs.find(c => c.id ===id);
+//     res.send(selectedChefs);
+// })
+
+// app.get('/chefs', (req, res) => {
+//     res.send( chefs );
+// })
+
+// app.listen(5000, ()=> {
+//     console.log('server is runnig on port 5000');
+// })
 const express= require('express');
 const cors = require('cors');
 
@@ -9,11 +34,17 @@ const chefs = require("./data.json");
 app.get('/', (req , res) => {
     res.json({ message: "hello Server"});
 });
+app.get('/chefs/:id', (req, res) => {
+    const id = req.params.id;
+    console.log(id);
+    const selectedChefs = chefs.find(c => c.id ===id);
+    res.send(selectedChefs);
+})
 
 app.get('/chefs', (req, res) => {
     res.send( chefs );
 })
 
 app.listen(5000, ()=> {
-    console.log('server is runnig on port 5000');
+    console.log('server is running on port 5000');
 })
