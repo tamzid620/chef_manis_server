5939bb9c92c179752c8f5ef37e988a1dcb18a83e
tamzid620 <tamzid620@gmail.com>
2023-05-02 11:41:55 +0600

    json data information added

--- index.js
+++ index.js
@@ -6,6 +6,10 @@ app.get('/', (req , res) => {
     res.json({ message: "hello Server"});
 });
 
+app.get('/allData', (req, res) => {
+    res.send({result: 'ok'})
+})
+
 app.listen(5000, ()=> {
     console.log('server is runnig on port 5000');
 })
